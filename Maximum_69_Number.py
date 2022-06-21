d302617971a29894129fc17c11211832bc073d54
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-06-21 14:34:13 +0530

    x=int(input()) a=list(str(x)) for i in range(len(a)):     if(a[i]=='6'):         a[i]='9'         break     else:         continue a=''.join(a) print(int(a))

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,12 +1,10 @@
 x=int(input())
-a=[]
-while(x>0):
-    r=x%10
-    a.append(r)
-    x=x//10
-for i in range(len(a)-1,-1,-1):
-    if a[i]==6:
-        a[i]=9
+a=list(str(x))
+for i in range(len(a)):
+    if(a[i]=='6'):
+        a[i]='9'
         break
-for i in range(len(a)-1,-1,-1):
-    print(a[i],end="")
+    else:
+        continue
+a=''.join(a)
+print(int(a))
