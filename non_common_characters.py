6ebad61db78f72bd2ddb79f8a56e29cd6e8493af
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-07-14 10:54:28 +0530

    s1=input().lower() s2=input().lower() d=[] for i in s1:     if(i!=' ' and i not in s2):         d.append(i) for i in s2:     if(i!=' ' and i not in s1):         d.append(i) d=sorted(set(d)) for i in d:     print(i,end="")

---
+++ non_common_characters.py
@@ -0,0 +1,12 @@
+s1=input().lower()
+s2=input().lower()
+d=[]
+for i in s1:
+    if(i!=' ' and i not in s2):
+        d.append(i)
+for i in s2:
+    if(i!=' ' and i not in s1):
+        d.append(i)
+d=sorted(set(d))
+for i in d:
+    print(i,end="")
