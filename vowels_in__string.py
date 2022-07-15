2c4fb036a6ef824a2c1a72e4e626b1d9e9db7129
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-07-15 19:37:00 +0530

    a=input() ch='aeuioAEUIO' c=0 d=[] for i in a:     if i in ch:         if(i not in d):             d.append(i)             c=1 if(c==0):     print('-1') else:     print(*d)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,11 +1,12 @@
 a=input()
-ch='aeiouAEIOU'
+ch='aeuioAEUIO'
 c=0
 d=[]
 for i in a:
-    if(i in ch and i not in d):
-        d.append(i)
-        c=1
+    if i in ch:
+        if(i not in d):
+            d.append(i)
+            c=1
 if(c==0):
     print('-1')
 else:
