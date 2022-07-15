e331cfed037419f18c15ebdfb7cd0cfef6545ab2
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-07-15 19:19:50 +0530

    a=input() ch1='aeuio' ch2='AEUIO' c=[] d=[] for i in a:     if i in ch1:         c.append(i) for i in a:     if i in ch2:         d.append(i) c1=len(list(set(c))) c2=len(list(set(d))) if(c1==5 or c2==5):     print('True') else:     print('False')

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,14 +1,17 @@
-x=input()
-ch="AEIOU"
-ch2="aeiou"
-d,d2=0,0
-for i in ch:
-    if i in x:
-        d+=1
-for i in ch2:
-    if i in x:
-        d2+=1
-if(d==5 or d2==5):
+a=input()
+ch1='aeuio'
+ch2='AEUIO'
+c=[]
+d=[]
+for i in a:
+    if i in ch1:
+        c.append(i)
+for i in a:
+    if i in ch2:
+        d.append(i)
+c1=len(list(set(c)))
+c2=len(list(set(d)))
+if(c1==5 or c2==5):
     print('True')
 else:
     print('False')
