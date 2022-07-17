f6f3692d48a2218dccfa7e5ce9bf465c4308732f
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-07-17 14:58:25 +0530

    a=input().split() a=sorted(a) a.sort(key=len) print(*a)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,15 +1,4 @@
-s = input().split()
-for i in range(len(s)-1):
-    if len(s[i])<len(s[i+1]):
-        continue
-    elif len(s[i])==len(s[i+1]):
-        a = min(s[i], s[i+1])
-        b = max(s[i], s[i+1])
-        s[i]=a
-        s[i+1]=b
-    elif len(s[i])>len(s[i+1]):
-        temp = s[i]
-        s[i] = s[i+1]
-        s[i+1] = temp
-print(*s)
-# print(*sorted(input().split()))
+a=input().split()
+a=sorted(a)
+a.sort(key=len)
+print(*a)
