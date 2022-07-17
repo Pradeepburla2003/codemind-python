ce5a9105927d941b6ce3fbccec395f60278e556f
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-07-17 14:54:41 +0530

    j=input() ch='aeuioAEUIO' ch2='qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM' c=0 d=len(j)-1 for k in range(len(j)//2):     if(j[k] in ch and j[d] in ch2):         c+=1     elif(j[k] in ch2 and j[d] in ch):         c+=1     d=d-1 print(c)

--- count_vowel_consonent_pairs.py
+++ count_vowel_consonent_pairs.py
@@ -1,12 +1,12 @@
-a=input()
+j=input()
 ch='aeuioAEUIO'
 ch2='qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM'
 c=0
-d=len(a)-1
-for i in range(len(a)//2):
-    if(a[i] in ch and a[d] in ch2):
+d=len(j)-1
+for k in range(len(j)//2):
+    if(j[k] in ch and j[d] in ch2):
         c+=1
-    elif(a[i] in ch2 and a[d] in ch):
+    elif(j[k] in ch2 and j[d] in ch):
         c+=1
     d=d-1
 print(c)
