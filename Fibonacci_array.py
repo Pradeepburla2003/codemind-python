48b372cc6b0d47a5854c451c355faad528de3c30
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-08-27 10:15:21 +0530

    n=int(input()) a=list(map(int,input().split())) if(n<3):     print('no') else:     for i in range(2,n):         if(a[i]!=a[i-1]+a[i-2]):             print('no')             break     else:         print('yes')

--- Fibonacci_array.py
+++ Fibonacci_array.py
@@ -1,10 +1,11 @@
 n=int(input())
-arr=list(map(int,input().split()))
+a=list(map(int,input().split()))
 if(n<3):
-    print("no")
+    print('no')
 else:
-    for i in range(2,len(arr)):
-        if(arr[i]!=arr[i-1]+arr[i-2]):
-            print("no")
-            quit()
-    print("yes")
+    for i in range(2,n):
+        if(a[i]!=a[i-1]+a[i-2]):
+            print('no')
+            break
+    else:
+        print('yes')
